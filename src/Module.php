e9858bc2245d74746e1bdff3c868e5212da73fa4
Le Phuong <phuong17889@gmail.com>
2023-01-09 16:47:25 +0700

    Update Module.php

--- src/Module.php
+++ src/Module.php
@@ -5,4 +5,13 @@ class Module extends \yii\debug\Module
 {
 
     public $traceLine = '<a href="phpstorm://open?url=file://{file}&line={line}">{text}</a>';
+
+	/**
+	 * {@inheritdoc}
+	*/
+	public function init()
+	{
+		parent::init();
+		$this->viewPath = '@vendor/yiisoft/yii2-debug/src/views';
+	}
 }
