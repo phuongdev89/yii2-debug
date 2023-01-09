85f64d3dfe74674d73674984934723d5b138d4b4
Le Phuong <phuong17889@gmail.com>
2023-01-09 16:32:56 +0700

    Update Module.php

--- src/Module.php
+++ src/Module.php
@@ -4,5 +4,5 @@ namespace phuong17889\debug;
 class Module extends \yii\debug\Module
 {
 
-    public $traceLine = '<a href="ide://open?url=file://{file}&line={line}">{text}</a>';
+    public $traceLine = '<a href="phpstorm://open?url=file://{file}&line={line}">{text}</a>';
 }
